59ba1e247a66aa73f8e0d471dec7a5523cd76d3c
moonbek <oybek4ik.mamatov.00@mail.ru>
2021-03-29 17:52:23 +0500

    feat: try chain problem solution

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,26 +1,38 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+  chain: "",
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return this.chain.split("~~").length();
   },
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (this.chain.length === 0) {
+      this.chain = ` ( ${value} )`;
+    } else {
+      this.chain += `~~( ${value} ) `;
+    }
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    let tempArray = this.chain.split("~~");
+    if (position < 0 || position >= tempArray.length) {
+      throw new Error();
+    }
+    tempArray.splice(position, 1);
+    this.chain = tempArray.join("~~");
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    let tempArray = this.chain.split("~~");
+    let newChainArray = [];
+    for (let i = tempArray.length - 1; i >= 0; i--) {
+      newChainArray.push(tempArray[i]);
+    }
+    this.chain = newChainArray.join("~~");
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }
+    const answer = this.chain;
+    this.chain = "";
+    return answer;
+  },
 };
 
 module.exports = chainMaker;
