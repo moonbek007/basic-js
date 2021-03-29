cf7aa2e37b52ea417e3131ccf330edd441048810
moonbek <oybek4ik.mamatov.00@mail.ru>
2021-03-29 17:11:36 +0500

    feat: solve what-season problem

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,21 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  if (date === undefined) {
+    return "Unable to determine the time of year!";
+  }
+  try {
+    const check = date.getMonth();
+  } catch (err) {
+    throw new Error(err);
+  }
+  if (date.getMonth() > 1 && date.getMonth() <= 4) {
+    return "winter";
+  } else if (date.getMonth() > 4 && date.getMonth() <= 7) {
+    return "spring";
+  } else if (date.getMonth() > 7 && date.getMonth() <= 10) {
+    return "autumn";
+  } else if (date.getMonth() > 10 || date.getMonth() <= 1) {
+    return "winter";
+  }
 };
